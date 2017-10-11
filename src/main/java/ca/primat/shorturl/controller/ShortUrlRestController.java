48ebc0d08e345c2a55f4cd99ea85a76d2b021854
Primat <dev.msp@gmail.com>
2017-10-11 14:59:40 -0400

    Cleanup

--- src/main/java/ca/primat/shorturl/controller/ShortUrlRestController.java
+++ src/main/java/ca/primat/shorturl/controller/ShortUrlRestController.java
@@ -3,15 +3,17 @@ package ca.primat.shorturl.controller;
 import ca.primat.shorturl.model.ShortUrl;
 import ca.primat.shorturl.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Rest interface for the Short URL entity
  */
 @RestController
+@RequestMapping("/api/v1/shorturl")
 public class ShortUrlRestController {
 
     private UrlService urlService;
@@ -21,18 +23,29 @@ public class ShortUrlRestController {
         this.urlService = urlService;
     }
 
+//    @GetMapping(value = "/api/v1/shorturl")
+//    public ResponseEntity<List<ShortUrl>> getAll(@RequestParam(value="url") String absoluteUrl) {
+//        return
+//    }
+
+
+//return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+
     /**
      * Handles a "get or create" type operation on ShortUrl
      * @param absoluteUrl The absolute URL to associate with the short URL
      * @return Returns a ShortUrl as JSON. If the maximum number of items has been reached, then a HTTP 403 is
      * returned. If the URL passed is invalid, returns a HTTP 400.
      */
-    @PostMapping(value = "/api/v1/shorturl")
+    @PostMapping(value = "")
     public ResponseEntity<ShortUrl> getOrCreate(@RequestParam(value="url") String absoluteUrl) {
 
         // TODO: Reject invalid URLs and add error handling
 
         ShortUrl shortUrl = urlService.getOrCreateShortUrl(absoluteUrl);
+
+        // TODO: Return HTTP 200 if object not created, otherwise return 201.
         return ResponseEntity.ok(shortUrl);
+        //return new ResponseEntity<ShortUrl>(shortUrl,HttpStatus.CREATED);
     }
 }
