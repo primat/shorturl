4edfac7a7d6087e965f008f7055f1ec3c8674052
Primat <dev.msp@gmail.com>
2017-10-14 16:45:12 -0400

    Removed application context dependence in unit tests

--- src/test/java/ca/primat/shorturl/service/ShortUrlServiceTest.java
+++ src/test/java/ca/primat/shorturl/service/ShortUrlServiceTest.java
@@ -2,6 +2,7 @@ package ca.primat.shorturl.service;
 
 import ca.primat.shorturl.model.ShortUrl;
 import ca.primat.shorturl.model.dao.ShortUrlRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -10,21 +11,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ShortUrlServiceTest {
 
-    @Autowired
     private ShortUrlService shortUrlService;
-    @MockBean
-    private Base62Service base62Service;
-    @MockBean
-    private ShortUrlRepository shortUrlRepository;
+    private Base62Service base62ServiceMock;
+    private ShortUrlRepository shortUrlRepositoryMock;
+
+    // Setup the mocks, dependencies and request context
+    @Before
+    public void setUp() {
+        base62ServiceMock = Mockito.mock(Base62Service.class);
+        shortUrlRepositoryMock = Mockito.mock(ShortUrlRepository.class);
+        shortUrlService = new ShortUrlService(base62ServiceMock, shortUrlRepositoryMock);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+    }
 
     @Test
     public void testIsValidSlug() {
@@ -44,13 +55,13 @@ public class ShortUrlServiceTest {
 
         // Test that the slug doesn't get overwritten
         shortUrl.setSlug("aaa");
-        Mockito.when(base62Service.encode(99L)).thenReturn("zZZ");
+        Mockito.when(base62ServiceMock.encode(99L)).thenReturn("zZZ");
         shortUrlService.assignSlug(shortUrl);
         assertThat(shortUrl.getSlug()).isEqualTo("aaa");
 
         // Test that the slug gets overwritten
         shortUrl.setId(10L);
-        Mockito.when(base62Service.encode(10L)).thenReturn("z");
+        Mockito.when(base62ServiceMock.encode(10L)).thenReturn("z");
         shortUrlService.assignSlug(shortUrl);
         assertThat(shortUrl.getSlug()).isEqualTo("z");
     }
@@ -58,15 +69,16 @@ public class ShortUrlServiceTest {
     @Test
     public void testGetOrCreate() {
         ShortUrl shortUrl = new ShortUrl("http://example.com");
+        shortUrl.setId(10L);
 
         // Test HttpStatus.CREATED (ShortUrl was created)
-        Mockito.when(shortUrlRepository.findByUrl(shortUrl.getUrl())).thenReturn(null);
-        Mockito.when(shortUrlRepository.save(any(ShortUrl.class))).thenReturn(null);
+        Mockito.when(shortUrlRepositoryMock.findByUrl(shortUrl.getUrl())).thenReturn(null);
+        Mockito.when(shortUrlRepositoryMock.save(any(ShortUrl.class))).thenReturn(null);
         ResponseEntity<ShortUrl> re = shortUrlService.getOrCreate(shortUrl);
         assertThat(re.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         // Test HttpStatus.OK (ShortUrl already exists)
-        Mockito.when(shortUrlRepository.findByUrl(shortUrl.getUrl())).thenReturn(shortUrl);
+        Mockito.when(shortUrlRepositoryMock.findByUrl(shortUrl.getUrl())).thenReturn(shortUrl);
         re = shortUrlService.getOrCreate(shortUrl);
         assertThat(re.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
